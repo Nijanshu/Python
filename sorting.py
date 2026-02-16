2da65a394ef7ac8e21da7b14cdb9c2c25e11b451
Nijanshu <nijanshusingh@gmail.com>
2026-02-17 02:19:32 +0530

    Sorting- Insertion

--- sorting.py
+++ sorting.py
@@ -6,16 +6,25 @@ def bubbleSort(arr):
                 arr[i],arr[i+1]=arr[i+1],arr[i]
 
 
-def selectionSort(arr,k):
+def selectionSort(arr):
         
     for i in range(len(arr)-1):
         smallest=min(arr[i:])
         sid=arr[i:].index(smallest)
         arr[i+sid],arr[i]=arr[i],arr[i+sid]
 
+def insertionSort(arr):
+    for i in range(1,len(arr)-1):
+        for j in range(i):
+            print(arr[i],arr[j])
+            if arr[i]<arr[j]:
+                arr[i],arr[j]=arr[j],arr[i]
+        print(arr)
         
         
-arr=[3,4,5,1,9,1,2,4]
+        
+arr=[3,2,5,1,9,1,2,4]
 # bubbleSort(arr)
-selectionSort(arr,0)
+# selectionSort(arr)
+insertionSort(arr)
 print(arr)
